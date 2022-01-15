7f8c0567fcc9a496a8d546eb7f12e854628ef4d4
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2022-01-15 20:41:24 -0300

    MovieCard

--- dsmovie/frontend/src/pages/Listing/index.tsx
+++ dsmovie/frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,47 @@
+//import MovieStar from "components/MovieStars";
+//import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
-
+import MovieCard from "components/MovieCard/indes";
 
 function Listing() {
     return (
-         < Pagination />
+        <>
+            < Pagination />
+
+            <div>
+                <div className="container">
+                    <div className="row">
+                        {/* col-sm-6 para cada card ocupar metade.. ai vou adapatando para cada padrão de tela */}
+                        <div className="col-sm-6 col-gg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-gg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-gg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-gg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-gg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-gg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-gg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                        <div className="col-sm-6 col-gg-4 col-xl-3 mb-3">
+                            <MovieCard />
+                        </div>
+                    </div>
+                </div>    
+
+            </div>
+        </>
     );
 }
 
-export default Listing;
+            export default Listing;
