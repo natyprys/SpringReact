20b2126f1824a0f253879abbd2de30159f06a99c
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2022-01-15 18:32:51 -0300

    Routes

--- dsmovie/frontend/src/App.tsx
+++ dsmovie/frontend/src/App.tsx
@@ -1,8 +1,32 @@
-import Navbar from "./components/Navbar";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+//componentes do react para configurar as rotas
+
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>  
+    {/* iniciar a configuração das rotas */}
+
+      <Navbar />
+      {/* toda pag vai ter o navbar */}
+
+      <Routes>
+        {/* aqui eu configuro rota por rota */}
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          
+          {/* subrota passando o parametro do filme escolhido com o id */}
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
