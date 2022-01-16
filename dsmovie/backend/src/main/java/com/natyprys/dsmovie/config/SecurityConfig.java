d009daba4af077b28a075f77d5d4d20f6071c291
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2022-01-16 13:00:37 -0300

    Security config

--- dsmovie/backend/src/main/java/com/natyprys/dsmovie/config/SecurityConfig.java
+++ dsmovie/backend/src/main/java/com/natyprys/dsmovie/config/SecurityConfig.java
@@ -1,5 +1,19 @@
 package com.natyprys.dsmovie.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.arrotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSEcurity;
+import org.springframework.security.config.annotation.web.configuration.EmableWebSecurity;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
